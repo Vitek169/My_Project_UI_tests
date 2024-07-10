aa115a9801fb6265c511b26620289dff148f01cf
Vitek169 <sambo911@mail.ru>
2024-07-10 16:45:42 +0300

    добавление страниц pyment_page, finish_pafe

--- base/base_class.py
+++ base/base_class.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Base():
 
     def __init__(self, driver):
@@ -8,8 +11,22 @@ class Base():
         get_url = self.dtiver.current_url
         print(f'Current Url {get_url}')
 
-    """ Method assert word"""
+    """Method assert word"""
     def assert_word(self, word, result):
         value_word = word.text
         assert value_word == result
         print('Good Value Word')
+
+    """Method screenshot"""
+    def get_screenshot(self):
+        now_date = datetime.datetime.now(datetime.UTC)
+        name_screenshot = 'screenshot' + str(now_date) + '.png'
+        self.dtiver.save_screenshot(f'screen/{name_screenshot}')
+
+    """ Method assert URL"""
+    def assert_url(self, result):
+        get_url = self.dtiver.current_url
+        assert get_url == result
+        print('Get Value URL')
+
+
--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -2,17 +2,16 @@ import datetime
 import time
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 from pages.cart_page import Cart_page
 from pages.client_inform_page import Client_inform_page
+from pages.finish_page import Finish_page
 from pages.login_page import Login_page
 from pages.main_page import Main_page
+from pages.payment_page import Payment_page
 
 
 def test_select_product():
@@ -23,11 +22,20 @@ def test_select_product():
 
     login = Login_page(driver)
     login.authorization()
+
     mp = Main_page(driver)
     mp.select_product()
+
     cp = Cart_page(driver)
     cp.click_checkout_button()
+
     cip = Client_inform_page(driver)
     cip.input_inform()
+
+    p = Payment_page(driver)
+    p.payment()
+
+    f = Finish_page(driver)
+    f.finish()
 
 
